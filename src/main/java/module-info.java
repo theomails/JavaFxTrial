633d57164138db4c084a3d5a3434b855e34f1d78
Theodore Ravindranath <theodore.ravindranath@redflex.com>
2026-01-05 00:03:40 +0530

    Final fix for modular Guava event bus to get JLink to work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.vrrm.javafxtrial {
     requires javafx.controls;
     requires javafx.graphics;
+    requires com.google.common;
     requires com.miglayout.javafx;
     requires static lombok;
 
