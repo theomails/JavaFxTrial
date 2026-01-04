51605f19eec7e6e6a42fd425318567b92f089e02
Theodore Ravindranath <theodore.ravindranath@redflex.com>
2026-01-04 23:45:58 +0530

    JavaFX jlink + JPMS experiment (EventBus, MigLayout)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.vrrm.javafxtrial {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires com.miglayout.javafx;
+    requires static lombok;
+
+    exports com.vrrm;
+}
